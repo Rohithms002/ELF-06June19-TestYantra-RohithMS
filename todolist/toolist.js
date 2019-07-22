9e716c0869dfbea157973e604d3c37745b7d919c
Rohithms002 <51232219+Rohithms002@users.noreply.github.com>
2019-07-22 13:59:18 +0530

    Add files via upload

---
+++ todolist/toolist.js
@@ -0,0 +1,30 @@
+function getValue(){
+    var form = document.forms[0];
+    var formlist= form.todolist.value;
+   
+
+// var em= document.createTextNode("List");
+// var pass= document.createTextNode("Remove");
+// var pwd=document.createTextNode(formlist);
+//     // =document.createElement("table");
+//     let table=document.createElement("table");
+//     let tr1= document.createElement("tr");
+//     let td1= document.createElement("td");
+//     let td2= document.createElement("td");
+//     td1.appendChild(em) ;
+
+//     td2.appendChild(pass);
+//     tr1.appendChild(td1);
+//     tr1.appendChild(td2);
+//     table.appendChild(tr1);
+
+//     let tr2= document.createElement("tr");
+//     let td3= document.createElement("td");
+//     td4.appendChild(pwd);
+//     tr2.appendChild(td3);
+//     tr2.appendChild(td4);
+//     table.appendChild(tr2);
+//     document.body.appendChild(table);
+
+  };
+
